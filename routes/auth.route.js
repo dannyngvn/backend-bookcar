3308a4ebb949786871499699942494c8d6165362
Danny <dannyngvn@gmail.com>
2024-03-10 11:54:24 +0700

    sua lại duong dan luu anh

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,7 +5,7 @@ import multer from 'multer';
 import { ObjectId } from 'mongodb';
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/');
+    cb(null, 'public');
   },
   filename: function (req, file, cb) {
     // Giữ lại phần mở rộng của file
