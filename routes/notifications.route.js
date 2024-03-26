07c6d862488dc77c452ff581720a3a4eaef2d02f
Danny <dannyngvn@gmail.com>
2024-03-27 00:57:49 +0700

    router noti

--- routes/notifications.route.js
+++ routes/notifications.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
-import { checkMoneyMiddleware } from '../middlewares/checkMoney.middleware.js';
+
 import { db } from '../db.js';
 
 const router = express.Router();
 
 router.get('/allpushtoken', async (req, res) => {
+  console.log('lay all token push');
   const token = await db.Users.find({}).toArray();
 
   const allpushtoken = token.map(user => user.pushToken);
